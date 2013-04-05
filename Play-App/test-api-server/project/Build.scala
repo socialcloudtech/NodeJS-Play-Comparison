cb8eb38fe068f5962d71fc461504981317173a77
sadik rupani <sadik.rupani@synerzip.com>
2013-04-05 14:46:47 +0530

    jdbc connection successfull and single route defined to work with.

--- Play-App/test-api-server/project/Build.scala
+++ Play-App/test-api-server/project/Build.scala
@@ -7,8 +7,8 @@ object ApplicationBuild extends Build {
     val appName         = "test-api-server"
     val appVersion      = "1.0-SNAPSHOT"
 
-    val appDependencies = Seq(
-      // Add your project dependencies here,
+    val appDependencies = Seq(    
+     "postgresql" % "postgresql" % "9.1-901.jdbc4"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
