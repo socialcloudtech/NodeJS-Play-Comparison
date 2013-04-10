68b782a5d6603f08e2677a9befb43a9a72ccd919
Nachiket Kakatkar <nachiket.kakatkar@synerzip.com>
2013-04-10 15:53:27 +0530

    Documenation for db utils

--- NodeJs-App/db/utils.js
+++ NodeJs-App/db/utils.js
@@ -1,6 +1,10 @@
 //utils : to get random date, string and real numbers.
 module.exports = {
-	//function to get random dte. for simplicity, chooses a random index from an array
+	/**
+	 * Get random date. for simplicity, chooses a random index from an array
+	 * @author nachiket
+	 * @version 0.1.0
+	 */
 	randomDate: function() {
 		var strings = ['2013-01-01', '2013-02-02', '2012-12-01', '2011-11-22'];
 		var randomI = Math.floor(Math.random() * 10) % strings.length;
@@ -8,7 +12,11 @@ module.exports = {
 		return strings[randomI];
    },
 
-	//function to get random string. for simplicity, chooses a random index from an array twice and returns concatenated string
+	/**
+	 * Get random string. for simplicity, chooses a random index from an array twice and returns concatenated string
+	 * @author nachiket
+	 * @version 0.1.0
+	 */
 	randomString: function() {
 		//taking human readable strings
 		var strings = ['abcdefghij', 'bcdefghija', 'cdefghijab', 'defghijabc', 'efghijabcd', 'fghijabcde', 'ghijabcdef', 'hijabcdefg', 'ijabcdefgh', 'jabcdefghi'];
@@ -17,7 +25,11 @@ module.exports = {
 		return strings[randomI1] + strings[randomI2];
 	},
 
-	//function to get a random real numnber
+	/**
+	 * Get a random real numnber
+	 * @author nachiket
+	 * @version 0.1.0
+	 */
 	randomReal: function() {
 		return Math.random() * 1000;
 	}
